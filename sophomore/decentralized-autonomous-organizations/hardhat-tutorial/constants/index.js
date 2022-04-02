2c5cf29337057046d5b95d2d86677201310afa29
ChrisiPK <ChrisiPK@gmail.com>
2022-04-02 19:35:01 +0200

    add DAO smart contracts

---
+++ sophomore/decentralized-autonomous-organizations/hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xAD84033D3997627451F005441A7B0F507a9E25D4";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
