570366638a34c04959a075f3b8fa38479637aa28
ChrisiPK <ChrisiPK@gmail.com>
2022-04-02 18:48:50 +0200

    add initial coin offering exercise

---
+++ sophomore/initial-coin-offering/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xAD84033D3997627451F005441A7B0F507a9E25D4";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
